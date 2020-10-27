bf78ad906b4c4403d0703166bc5806e1e5371ca7
dimstar <kylermagee@gmail.com>
2020-10-26 22:27:19 -0700

    uses main layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Main from '../components/layouts';
 
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <Main>
+      <div>Hello world!</div>
+    </Main>
+  );
 }
