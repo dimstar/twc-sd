2a5a90eb62e71e2e336e0ca9c25d24ffcb6c4d60
dimstar <kylermagee@gmail.com>
2020-10-31 14:39:14 -0700

    simplifies env var usage

--- functions/handleJoinForm.js
+++ functions/handleJoinForm.js
@@ -1,20 +1,11 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
+const authKey = JSON.parse(process.env.JOIN_SHEET_KEY);
+
 exports.handler = async (event, context, callback) => {
   try {
     const doc = new GoogleSpreadsheet(process.env.JOIN_SHEET_ID);
-    await doc.useServiceAccountAuth({
-      type: process.env.TYPE,
-      project_id: process.env.PROJECT_ID,
-      private_key_id: process.env.PRIAVTE_KEY_ID,
-      private_key: process.env.PRIVATE_KEY,
-      client_email: process.env.CLIENT_EMAIL,
-      client_id: process.env.CLIENT_ID,
-      auth_uri: process.env.AUTH_URI,
-      token_uri: process.env.TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
-    });
+    await doc.useServiceAccountAuth(authKey, data => console.log(data));
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
 
